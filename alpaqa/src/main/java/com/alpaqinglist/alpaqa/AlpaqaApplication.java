4ffd691cc8985c13e8296c8cc8a662731f8dc2d6
slere <ensle.rene@gmail.com>
2020-05-26 10:35:26 +0200

    basic spring boot setup

---
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/AlpaqaApplication.java
@@ -0,0 +1,13 @@
+package com.alpaqinglist.alpaqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpaqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpaqaApplication.class, args);
+	}
+
+}
---
+++ alpaqa/src/test/java/com/alpaqinglist/alpaqa/AlpaqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpaqinglist.alpaqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpaqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
