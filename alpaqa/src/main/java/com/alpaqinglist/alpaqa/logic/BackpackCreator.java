1e2cf607be3545627b3d262106a302a4cc70ece0
Alex3m <apetrov8911@gmail.com>
2020-05-27 11:01:52 +0200

    renew BC

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/logic/BackpackCreator.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/logic/BackpackCreator.java
@@ -1,22 +1,23 @@
 package com.alpaqinglist.alpaqa.logic;
 
+import com.alpaqinglist.alpaqa.data.BackpackDTO;
 import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
 import com.alpaqinglist.alpaqa.persistence.repository.BackpackRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BackpackCreator {
-    private final BackpackTranslater translator;
+    private final BackpackTranslator translator;
     private final BackpackRepository repository;
 
-    public BackpackCreator(BackpackTranslater translator, BackpackRepository repository) {
+    public BackpackCreator(BackpackTranslator translator, BackpackRepository repository) {
         this.translator = translator;
         this.repository = repository;
     }
 
     public BackpackDTO create(BackpackDTO backpackDTO) {
-        Backpack backpack = translator.translateFromDTO(backpackDTO);
-        Backpack backpackSaved = repository.save(backpackDTO);
-        return translator.translateToDTO(backpackSaved);
+        Backpack backpack = translator.translateToBackpack(backpackDTO);
+        Backpack backpackSaved = repository.save(backpack);
+        return translator.translateToBackpackTDO(backpackSaved);
     }
 }
