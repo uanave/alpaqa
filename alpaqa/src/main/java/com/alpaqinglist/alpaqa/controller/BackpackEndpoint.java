f0730c4bdd7361a053ca0c587dc8de092c3ec2b0
slere <ensle.rene@gmail.com>
2020-05-27 11:57:40 +0200

    controller,backpackTDO changes

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackEndpoint.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackEndpoint.java
@@ -1,6 +1,8 @@
 package com.alpaqinglist.alpaqa.controller;
 
 import com.alpaqinglist.alpaqa.data.BackpackDTO;
+import com.alpaqinglist.alpaqa.logic.BackpackCreator;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class BackpackEndpoint {
     private final BackpackCreator backpackCreator;
 
+    public BackpackEndpoint(BackpackCreator backpackCreator) {
+        this.backpackCreator = backpackCreator;
+    }
+    @PostMapping
     BackpackDTO createNewBackpack(@RequestBody BackpackDTO backpackDTO){
 
         return backpackCreator.create(backpackDTO);
--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/data/BackpackDTO.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/data/BackpackDTO.java
@@ -1,9 +1,69 @@
 package com.alpaqinglist.alpaqa.data;
 
+import java.util.Objects;
+
 public class BackpackDTO {
 
     private Long id;
     private String name;
     private String description;
     private String type;
+
+    public BackpackDTO(Long id, String name, String description, String type) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+    }
+
+    public BackpackDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BackpackDTO that = (BackpackDTO) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, type);
+    }
 }
