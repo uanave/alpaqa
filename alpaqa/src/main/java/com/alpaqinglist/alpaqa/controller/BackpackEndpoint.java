cb5e122178a2f79d26aa977cd049a21b6318b328
slere <ensle.rene@gmail.com>
2020-05-28 11:36:52 +0200

    Errorhandling in BackpackEndpoint

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackEndpoint.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackEndpoint.java
@@ -3,9 +3,12 @@ package com.alpaqinglist.alpaqa.controller;
 import com.alpaqinglist.alpaqa.data.BackpackDTO;
 import com.alpaqinglist.alpaqa.logic.BackpackCreator;
 import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/backpack")
@@ -17,9 +20,11 @@ public class BackpackEndpoint {
     }
 
     @PostMapping
-    BackpackDTO createNewBackpack(@RequestBody BackpackDTO backpackDTO) {
+    ResponseEntity<BackpackDTO> createNewBackpack(@RequestBody BackpackDTO backpackDTO) {
 
-        return backpackCreator.create(backpackDTO).orElseGet(null);
+        Optional<BackpackDTO> oBackpack = backpackCreator.create(backpackDTO);
+        return oBackpack.map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.BAD_REQUEST));
 
     }
 
