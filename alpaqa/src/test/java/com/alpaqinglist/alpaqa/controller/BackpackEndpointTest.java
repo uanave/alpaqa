685ff6329cefec1be8703d59b229f41c91413bae
uanave <uanavasile@gmail.com>
2020-05-28 09:35:10 +0200

    format code

--- alpaqa/src/test/java/com/alpaqinglist/alpaqa/controller/BackpackEndpointTest.java
+++ alpaqa/src/test/java/com/alpaqinglist/alpaqa/controller/BackpackEndpointTest.java
@@ -2,21 +2,16 @@ package com.alpaqinglist.alpaqa.controller;
 
 import com.alpaqinglist.alpaqa.data.BackpackDTO;
 import com.alpaqinglist.alpaqa.logic.BackpackCreator;
-import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
-import com.alpaqinglist.alpaqa.persistence.repository.BackpackRepository;
-import com.alpaqinglist.alpaqa.persistence.repository.ItemRepository;
-import com.alpaqinglist.alpaqa.persistence.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackpackEndpointTest {
 
@@ -26,20 +21,16 @@ class BackpackEndpointTest {
     @MockBean
     BackpackCreator backpackCreator;
 
-
-
-
     @Value("${test.backpackEndpoint.url}")
     String url;
 
     @Test
     void createNewBackpack() {
-
         BackpackDTO backpackTDO = new BackpackDTO();
-        testRestTemplate.postForObject(url,backpackTDO,void.class);
 
-       Mockito.verify(backpackCreator).create(Mockito.any());
+        testRestTemplate.postForObject(url, backpackTDO, void.class);
 
+        verify(backpackCreator).create(Mockito.any());
 
 
     }
