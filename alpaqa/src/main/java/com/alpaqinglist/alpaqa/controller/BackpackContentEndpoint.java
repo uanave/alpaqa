e7f226015d134b29d6d10e2bbf6404f380aa8780
uanave <uanavasile@gmail.com>
2020-06-08 13:51:01 +0200

    add endpoint for editing and getting an Item

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackContentEndpoint.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackContentEndpoint.java
@@ -1,11 +1,11 @@
 package com.alpaqinglist.alpaqa.controller;
 
 import com.alpaqinglist.alpaqa.logic.BackpackService;
+import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
 import com.alpaqinglist.alpaqa.persistence.domain.Item;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alpaqinglist.alpaqa.persistence.repository.BackpackRepository;
+import com.alpaqinglist.alpaqa.persistence.repository.ItemRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,13 +13,28 @@ import java.util.List;
 @RequestMapping("/backpacks")
 public class BackpackContentEndpoint {
     private final BackpackService service;
+    private final ItemRepository itemRepository;
+    private final BackpackRepository backpackRepository;
 
-    public BackpackContentEndpoint(BackpackService service) {
+    public BackpackContentEndpoint(BackpackService service, ItemRepository itemRepository, BackpackRepository backpackRepository) {
         this.service = service;
+        this.itemRepository = itemRepository;
+        this.backpackRepository = backpackRepository;
     }
 
     @GetMapping("{id}/items")
     List<Item> getItems(@PathVariable Long id) {
         return service.backpackItemsPreview(id);
+    }
+
+    @GetMapping("{id}/items/{itemId}")
+    Item getItem(@PathVariable Long itemId) {
+        return itemRepository.findById(itemId).orElse(null);
+    }
+
+    @PutMapping("/{backpackId}/items/{itemId}")
+    Backpack updateBackpack(@PathVariable Long backpackId, @RequestBody Item item) {
+        itemRepository.save(item);
+        return backpackRepository.findById(backpackId).orElse(null);
     }
 }
