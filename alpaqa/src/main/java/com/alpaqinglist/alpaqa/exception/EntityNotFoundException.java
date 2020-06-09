c3d76299f5c6bc02afad101d4850ed996ed1ca90
uanave <uanavasile@gmail.com>
2020-06-09 11:49:48 +0200

    add repos to test

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/exception/EntityNotFoundException.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/exception/EntityNotFoundException.java
@@ -1,12 +1,7 @@
 package com.alpaqinglist.alpaqa.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-
-
-
-    public EntityNotFoundException(String message){
-
+    public EntityNotFoundException(String message) {
         super(message);
     }
-
 }
