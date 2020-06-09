ec45de7bdb066337c4af0bba5bd802e7e4490be3
uanave <uanavasile@gmail.com>
2020-06-09 11:50:14 +0200

    add repos to test

--- alpaqa/src/test/java/com/alpaqinglist/alpaqa/logic/BackpackDeleterTest.java
+++ alpaqa/src/test/java/com/alpaqinglist/alpaqa/logic/BackpackDeleterTest.java
@@ -1,10 +1,14 @@
 package com.alpaqinglist.alpaqa.logic;
 
+import com.alpaqinglist.alpaqa.AlpaqaApplication;
 import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
 import com.alpaqinglist.alpaqa.persistence.repository.BackpackRepository;
+import com.alpaqinglist.alpaqa.persistence.repository.ItemRepository;
+import com.alpaqinglist.alpaqa.persistence.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -17,7 +21,12 @@ class BackpackDeleterTest {
     BackpackDeleter backpackDeleter;
     @MockBean
     BackpackRepository repository;
+    @MockBean
+    ItemRepository itemRepository;
+    @MockBean
+    UserRepository userRepository;
     Long backpackID = 1L;
+
     @Test
     void delete() {
         Mockito.when(repository.findById(backpackID)).thenReturn(Optional.of(new Backpack()));
