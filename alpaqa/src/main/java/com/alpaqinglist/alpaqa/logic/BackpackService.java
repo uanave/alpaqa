8675d79a844ee15635d3948ad7b301004b026dfc
Alex3m <apetrov8911@gmail.com>
2020-06-09 09:14:13 +0200

    changes to BS

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/logic/BackpackService.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/logic/BackpackService.java
@@ -1,8 +1,10 @@
 package com.alpaqinglist.alpaqa.logic;
 
+import com.alpaqinglist.alpaqa.exception.EntityNotFoundException;
 import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
 import com.alpaqinglist.alpaqa.persistence.domain.Item;
 import com.alpaqinglist.alpaqa.persistence.repository.BackpackRepository;
+import com.alpaqinglist.alpaqa.persistence.repository.ItemRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,18 +13,30 @@ import java.util.Optional;
 
 @Service
 public class BackpackService {
-    private final BackpackRepository repository;
+    private final BackpackRepository backpackRepository;
+    private final ItemRepository itemRepository;
 
-    public BackpackService(BackpackRepository repository) {
-        this.repository = repository;
+    public BackpackService(BackpackRepository backpackRepository, ItemRepository itemRepository) {
+        this.backpackRepository = backpackRepository;
+        this.itemRepository = itemRepository;
     }
 
     public List<Item> backpackItemsPreview(Long backpackID) {
-        Optional<Backpack> oBackpack = repository.findById(backpackID);
+        Optional<Backpack> oBackpack = backpackRepository.findById(backpackID);
         List<Item> items = new ArrayList<>();
         if (oBackpack.isPresent()) {
             items = oBackpack.get().getItems();
         }
         return items;
+    }
+
+    public Item updateItem(Long itemId, Item item) {
+        Optional<Item> oItem = itemRepository.findById(itemId);
+        if (oItem.isPresent()) {
+            item.setId(itemId);
+            return itemRepository.save(item);
+        } else {
+            throw new EntityNotFoundException("No item found");
+        }
     }
 }
--- alpaqa/src/test/java/com/alpaqinglist/alpaqa/controller/BackpackContentEndpointTest.java
+++ alpaqa/src/test/java/com/alpaqinglist/alpaqa/controller/BackpackContentEndpointTest.java
@@ -1,15 +1,22 @@
 package com.alpaqinglist.alpaqa.controller;
 
+import com.alpaqinglist.alpaqa.exception.EntityNotFoundException;
 import com.alpaqinglist.alpaqa.logic.BackpackService;
 import com.alpaqinglist.alpaqa.persistence.domain.Item;
+import com.alpaqinglist.alpaqa.persistence.repository.BackpackRepository;
+import com.alpaqinglist.alpaqa.persistence.repository.ItemRepository;
+import com.alpaqinglist.alpaqa.persistence.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -18,13 +25,62 @@ class BackpackContentEndpointTest {
     TestRestTemplate restTemplate;
     @MockBean
     BackpackService service;
+    @MockBean
+    ItemRepository itemRepository;
+    @MockBean
+    BackpackRepository backPackRepository;
+    @MockBean
+    UserRepository userRepository;
 
     Long backpackID = 1L;
     String url = "/backpacks/" + backpackID + "/items";
+    Long itemId = 1L;
+    String itemUrl = url + "/" + itemId;
+
+
+    Item item = new Item();
 
     @Test
     void getItems() {
         restTemplate.getForObject(url, Item[].class);
-        Mockito.verify(service).backpackItemsPreview(backpackID);
+        verify(service).backpackItemsPreview(backpackID);
     }
+
+    @Test
+    void getExistingItem() {
+        Item expected = item;
+        expected.setId(itemId);
+        when(itemRepository.findById(itemId))
+                .thenReturn(Optional.of(item));
+        when(itemRepository.save(item))
+                .thenReturn(item);
+        restTemplate.getForObject(itemUrl, Item.class);
+        assertEquals(expected, item);
+        verify(itemRepository).findById(itemId);
+        verify(itemRepository).save(item);
+    }
+
+    @Test
+    void getNonExistingItem() {
+        when(itemRepository.findById(itemId))
+                .thenReturn(Optional.empty());
+        restTemplate.getForObject(itemUrl, null);
+        verify(itemRepository).findById(itemId);
+    }
+
+    @Test
+    void updateItemIfPresent() {
+        when(itemRepository.findById(itemId))
+                .thenReturn((Optional.of(item)));
+        restTemplate.put(itemUrl, item ,Item.class);
+        verify(itemRepository).findById(itemId);
+    }
+    @Test
+    void updateItemIfNotPresent() {
+        when(itemRepository.findById(itemId))
+                .thenReturn(Optional.empty());
+        restTemplate.put(itemUrl, item ,void.class);
+        verify(itemRepository).findById(itemId);
+    }
+
 }
