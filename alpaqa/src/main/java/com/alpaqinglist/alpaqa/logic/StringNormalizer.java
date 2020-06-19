d4a6f9f3d885542468b48a087e6a7ece9357d1f9
uanave <uanavasile@gmail.com>
2020-06-19 13:28:08 +0200

    add StringNormalizer

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/logic/StringNormalizer.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/logic/StringNormalizer.java
@@ -9,8 +9,8 @@ public class StringNormalizer {
     private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
 
     public static String toSlug(String input) {
-        String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
-        String normalized = Normalizer.normalize(nowhitespace, Normalizer.Form.NFD);
+        String noWhitespace = WHITESPACE.matcher(input).replaceAll("-");
+        String normalized = Normalizer.normalize(noWhitespace, Normalizer.Form.NFD);
         String slug = NONLATIN.matcher(normalized).replaceAll("");
         return slug.toLowerCase(Locale.ENGLISH);
     }
