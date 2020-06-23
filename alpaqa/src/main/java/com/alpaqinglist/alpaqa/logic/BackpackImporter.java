05ae55d8cb72461ed4dcda56f69ad5d839ea4970
Alex3m <apetrov8911@gmail.com>
2020-06-23 09:22:32 +0200

    BackpackImporter

---
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/logic/BackpackImporter.java
@@ -0,0 +1,63 @@
+package com.alpaqinglist.alpaqa.logic;
+
+import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
+import com.alpaqinglist.alpaqa.persistence.domain.Item;
+import com.alpaqinglist.alpaqa.persistence.repository.BackpackRepository;
+import com.alpaqinglist.alpaqa.persistence.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BackpackImporter {
+    private final BackpackRepository backpackRepository;
+    private final ItemRepository itemRepository;
+    private final BackpackExtractor backpackExtractor;
+    private int backpackNameSequence;
+    private int sequenceStep;
+
+    public BackpackImporter(BackpackRepository backpackRepository, ItemRepository itemRepository, BackpackExtractor backpackExtractor,
+                            @Value("${backpack.details.backpackNameSequence}") int backpackNameSequence,
+                            @Value("${backpack.details.sequenceStep}") int sequenceStep) {
+        this.backpackRepository = backpackRepository;
+        this.itemRepository = itemRepository;
+        this.backpackExtractor = backpackExtractor;
+        this.backpackNameSequence = backpackNameSequence;
+        this.sequenceStep = sequenceStep;
+    }
+
+    public Optional<Backpack> backpackSavior(MultipartFile file) {
+        Optional<Backpack> oBackpack = backpackExtractor.extractFromMultipartFile(file);
+        if (oBackpack.isEmpty()) {
+            return oBackpack;
+        }
+        Backpack backpack = oBackpack.get();
+        backpack.setId(null);
+        do {
+            String backpackName = backpack.getName();
+            if (!backpackRepository.existsByName(backpackName)) {
+                return preparingAndSaving(backpack);
+            }
+            String nameWithNextNumber = nameGenerator(backpackName);
+            backpack.setName(nameWithNextNumber);
+        } while (true);
+    }
+
+
+    Optional<Backpack> preparingAndSaving(Backpack backpack) {
+        List<Item> items = backpack.getItems();
+        items.forEach(item -> item.setId(null));
+        items = itemRepository.saveAll(items);
+        backpack.setItems(items);
+        return Optional.of(backpackRepository.save(backpack));
+    }
+
+    String nameGenerator(String backpackName) {
+        backpackName = backpackName + backpackNameSequence;
+        backpackNameSequence += sequenceStep;
+        return backpackName;
+    }
+}
