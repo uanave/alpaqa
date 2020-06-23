6df377813d0f6d7ff5491fea8e83c3a9442efb16
Alex3m <apetrov8911@gmail.com>
2020-06-23 09:25:37 +0200

    small changes for endpoint backpack export

--- alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackEndpoint.java
+++ alpaqa/src/main/java/com/alpaqinglist/alpaqa/controller/BackpackEndpoint.java
@@ -2,10 +2,12 @@ package com.alpaqinglist.alpaqa.controller;
 
 import com.alpaqinglist.alpaqa.data.error.ApiError;
 import com.alpaqinglist.alpaqa.logic.BackpackEditor;
+import com.alpaqinglist.alpaqa.logic.BackpackImporter;
 import com.alpaqinglist.alpaqa.persistence.domain.Backpack;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -15,9 +17,11 @@ import java.util.Optional;
 @RequestMapping("/backpacks")
 public class BackpackEndpoint {
     private final BackpackEditor backpackEditor;
+    private final BackpackImporter backpackImporter;
 
-    public BackpackEndpoint(BackpackEditor backpackEditor) {
+    public BackpackEndpoint(BackpackEditor backpackEditor, BackpackImporter backpackImporter) {
         this.backpackEditor = backpackEditor;
+        this.backpackImporter = backpackImporter;
     }
 
     @PostMapping
@@ -45,5 +49,13 @@ public class BackpackEndpoint {
     @GetMapping
     List<Backpack> getAllBackpacks() {
         return backpackEditor.getAll();
+    }
+
+    @PostMapping("/import-backpack")
+    ResponseEntity<Object> importBackpack(@RequestBody MultipartFile file) {
+        Optional<Backpack> oBackpack = backpackImporter.backpackSavior(file);
+
+        return oBackpack.map(dto -> new ResponseEntity<>((Object) dto, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(new ApiError(HttpStatus.BAD_REQUEST, "Error while importing backpack! The file is corrupted or not accessible."), HttpStatus.BAD_REQUEST));
     }
 }
